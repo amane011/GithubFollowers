26901286df366fa7bf4b0669fe8d540ff109e67c
amane011 <123193869+amane011@users.noreply.github.com>
2023-10-28 13:29:51 -0700

    Initial Commit

---
+++ GithubFollowers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GithubFollowers
+//
+//  Created by Ankit  Mane on 10/28/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
